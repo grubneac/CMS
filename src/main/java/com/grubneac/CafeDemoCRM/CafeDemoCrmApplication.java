ed6b2bbd7c1a6281de4175992b00e8eea4f29933
Grubneac <grubneac@yandex.ru>
2023-11-11 15:05:29 +0200

    add in compose.yaml for postgresql and flyway

---
+++ src/main/java/com/grubneac/CafeDemoCRM/CafeDemoCrmApplication.java
@@ -0,0 +1,13 @@
+package com.grubneac.CafeDemoCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeDemoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeDemoCrmApplication.class, args);
+	}
+
+}
