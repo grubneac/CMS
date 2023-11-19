0b278ddf6000ee6690918d43c2ba23725793e423
Grubneac <grubneac@yandex.ru>
2023-11-19 13:42:33 +0200

    add simple example PUT, POST, DELETE to person table

--- README.md
+++ README.md
@@ -15,13 +15,23 @@ This project will help to manage flow(customers orders, cooking flow, deposit mo
 <ul>
   <li>Download and install Docker</li>
   <li>Pull my project</li>
-  <li>Run script from (compose.yaml)</li>
+  <li>Run scripts from (compose.yaml)</li>
   <li>mvn clean install</li>
   <li>Run Spring Boot application</li>
-  <li>from CMD run script 'curl http://localhost:8080/api/persons'</li>
+  <li>Download Postman and run script by it or from CMD run script:</li>
     <ul>
-        <li>Download Postman and run script by it</li>
+        <li>'curl http://localhost:8080/api/persons'</li>
+        <li>'curl -X POST http://localhost:8080/api/persons -H 'Content-Type: application/json' -d '{"id": 1001, "firstName": "Bevan1", "lastName": "Beech1"}'</li>
     </ul>
+</ul>
+
+##Tools
+<ul>
+    <li>Docker Desktop</li>
+    <li>IntelliJ IDEA(Community Edition)</li>
+    <li>DBeaver</li>
+    <li>Postman</li>
+    <li>Git</li>
 </ul>
 
 
--- src/main/java/com/grubneac/CafeDemoCRM/controller/PersonController.java
+++ src/main/java/com/grubneac/CafeDemoCRM/controller/PersonController.java
@@ -4,10 +4,8 @@ import com.grubneac.CafeDemoCRM.model.Person;
 import com.grubneac.CafeDemoCRM.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,11 +22,11 @@ public class PersonController {
     PersonRepository personRepository;
 
     @GetMapping("/persons")
-    public ResponseEntity<List<Person>> getAllPersons(){
+    public ResponseEntity<List<Person>> getAllPersons() {
         try {
             List<Person> allPersons = personRepository.findAll();
-            if (Objects.requireNonNull(allPersons).isEmpty()){
-                return new ResponseEntity(NO_CONTENT);
+            if (Objects.requireNonNull(allPersons).isEmpty()) {
+                return new ResponseEntity<>(NO_CONTENT);
             } else {
                 return ResponseEntity.ok(allPersons);
             }
@@ -36,4 +34,36 @@ public class PersonController {
             return new ResponseEntity<>(null, INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/persons/{id}")
+    public ResponseEntity<Person> getPersonNyId(@PathVariable("id") Integer personId) throws Exception {
+        Person person = personRepository.findById(personId).
+                orElseThrow(() -> new Exception("Person " + personId + " not found"));
+        return ResponseEntity.ok(person);
+    }
+
+    @PostMapping("/persons")
+    public Person createPerson(@Validated @RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @PutMapping("/persons/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable("id") Integer personId,
+                                               @Validated @RequestBody Person personDetail) throws Exception {
+        Person person = personRepository.findById(personId).
+                orElseThrow(() -> new Exception("Person " + personId + " not found"));
+        person.setFirstName(personDetail.getFirstName());
+        person.setLastName(personDetail.getLastName());
+        Person updatedPerson = personRepository.save(person);
+        return ResponseEntity.ok(updatedPerson);
+    }
+
+    @DeleteMapping("persons/{id}")
+    public Boolean deletePerson(@PathVariable("id") Integer personId) throws Exception {
+        Person person = personRepository.findById(personId).
+                orElseThrow(() -> new Exception("Person " + personId + " not found"));
+        personRepository.delete(person);
+        return true;
+    }
+
 }
